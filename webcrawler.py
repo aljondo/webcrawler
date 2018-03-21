0413efd28fe773e01ef22780f635086d0d1d84e0
Austin Kilduff <kilduff.a@husky.neu.edu>
2018-03-21 17:22:56 -0400

    Cleaned up a bit

--- webcrawler.py
+++ webcrawler.py
@@ -26,12 +26,13 @@ secret_flags = []
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, 80))
 
+# Given a link, make sure it is relative and not absolute
 def getSafeLink(url):
     if url.startswith(root_site):
         return url[len(root_site):]
     return url
 
-#sets cookies and also returns the status
+# Given an HTTP response, set the appropriate cookies and return the status
 def checkResponseHeader(response):
     global s, csrftoken, sessionid, location
     if 'Set-Cookie:' in response:
@@ -43,24 +44,21 @@ def checkResponseHeader(response):
             elif line.startswith('Location:'):
                 location = line.split(" ")[1]
     status = response.split("\r")[0].split(" ")
-    # print(" c h e c k r e p o n s e h e a d er ")
-    #print(response)
+
     if len(status) >= 3:
-        # print(response.split("\r\n\r\n")[0]) #<- print header in case somethings weird
         status = status[1]
     else:
-        # print("something is going wrong here. this is the response:")
-        # print(response)
         status = " ".join(status)
-        # status = "something_is_wrong"
         s.close()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, 80))
     return status
 
+# Given a relative path, send a GET request for the page and return the BeautifulSoup object of that page
 def getRequest(route):
     global csrftoken, sessionid, s
-    #add cookies to request if they're there
+
+    # Add cookies to request if they're there
     cookie = "in"
     if len(csrftoken) > 0:
         cookie = "Cookie: csrftoken=" + csrftoken + "; sessionid=" + sessionid + "\r\n"
@@ -70,38 +68,26 @@ def getRequest(route):
     else:
         request = ('GET ' + route + ' HTTP/' + http_version + '\r\n'
                     'Host: ' + host + '\r\n\r\n')
-    # print('GET request:')
-    # print(request)
+
     s.send(request.encode('ascii'))
     response = s.recv(4096).decode('ascii')
+
+    # Check for bad responses
     while response == '0\r\n\r\n':
-        print('~~~~~~~~~~~~~~~~~~~~~~~~Bad response - resending request~~~~~~~~~~~~~~~~~~~~~~~~')
         s.close()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, 80))
         s.send(request.encode('ascii'))
         response = s.recv(4096).decode('ascii')
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~GET response~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    #print(response)
 
     status = checkResponseHeader(response)
 
-    stats = route + ': '
-    stats += 'chunked; ' if 'Transfer-Encoding: chunked' in response else 'not chunked; '
-    stats += 'complete; ' if '</html>' in response else 'incomplete; '
-    stats += status
-    #print(stats)
-
     # If the transfer is chunked and incomplete, keep receiving (as long as the status is 200)
     while 'Transfer-Encoding: chunked' in response and '</html>' not in response and status == '200':
-        #print('reading more...')
         response += s.recv(4096).decode('ascii')
         status = checkResponseHeader(response)
 
-    #print("WHAT IS THE STATUS OF THIS GET REQUEST")
-    #print(status)
     if status == '200':
-        #add route to explored
         explored.append(route)
     elif status == '301':
         explored.append(route)
@@ -113,31 +99,27 @@ def getRequest(route):
             return getRequest(unexplored.pop())
     elif status == '403' or status == '404':
         if len(unexplored) == 0:
-            #this means there's nothing left to explore. return False so the crawler knows
+            # This means there's nothing left to explore. return False so the crawler knows
             return False
         else:
-            #add route to explored, get the next route from the frontier and get that
+            # Add route to explored, get the next route from the frontier and get that
             explored.append(route)
             return getRequest(unexplored.pop())
     else:
-        #this means the status is 500, or something's going weirdly wrong.
-        #in both cases, we wanna try again with the same route anyway
+        # This means the status is 500, or something's going weirdly wrong.
+        # In both cases, we wanna try again with the same route anyway
         return getRequest(route)
     html = response.split('\r\n\r\n', 1)[-1]
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-def getLoginToken():
-    login_soup = getRequest(getSafeLink(login_page))
-    token = login_soup.find_all(attrs={"name": "csrfmiddlewaretoken"})[0]['value']
-    return token
-
-
+# Login to Fakebook through a POST request
 def postLogin():
-    # token = getLoginToken()
-    # content = 'username=' + username + '&password=' + password + '&csrfmiddlewaretoken=' + token + '&next=/fakebook/'
     global s
+    # Make an intial GET request to set the cookies
     getRequest(getSafeLink(login_page))
+
+    # Set up the POST request
     content = 'username=' + username + '&password=' + password + '&csrfmiddlewaretoken=' + csrftoken + '&next=/fakebook/'
     request =  ('POST ' + getSafeLink(login_page) + ' HTTP/' + http_version + '\n'
                 'Host: ' + host + '\n'
@@ -147,19 +129,15 @@ def postLogin():
                 '' + content + '\n\n')
     s.send(request.encode('ascii'))
     response = s.recv(4096).decode('ascii')
-    #print("POST response")
-    #print(response)
+
     status = checkResponseHeader(response)
+
     if status == '302':
-        #print("we made it")
         logged_in = True
     else:
-        #print("trying again")
         postLogin()
     return True
 
-# getRequest(getSafeLink(login_page))
-# On 200, get current page (should be root_page) and add it to unexplored
 
 def crawl():
     while len(unexplored) > 0:
@@ -167,64 +145,30 @@ def crawl():
         current_page = unexplored.pop()
         soup = getRequest(current_page)
         if not soup:
-            #got a 404, and there are no more links in the frontier
+            # Got a 404, and there are no more links in the frontier
             break
-        # while len(soup) <= 1:
-        #     soup = getRequest(current_page)
-        # print("THE SOUP")
-        # print(soup)
+
+        # Extract and handle <a> tags
         anchors = soup.find_all('a')
-        h2s = soup.find_all('h2')
-
-        print("Page:", current_page)
-        #print("anchors")
-        #print(anchors)
-        #print("headers")
-        #print(h2s)
-
         for a in anchors:
             if a['href'] not in explored and a['href'] not in unexplored:
                 if a['href'][:1] == '/' or a['href'][:25] == 'http://fring.ccs.neu.edu/':
                     unexplored.append(a['href'])
-        #print("new frontier")
-        #print(unexplored)
+
+        # Extract and handle <h2> tags (to find flags)
+        h2s = soup.find_all('h2')
         for h in h2s:
             text = h.contents[0]
             if 'FLAG:' in text:
-                print(" >>>>>>>>>>>>>>>>> CAUGHT ONE <<<<<<<<<<<<<<<<<<<<<< ")
                 flag = text.split(" ")[1]
-                secret_flags.append(flag)
-                with open("secret_flags", "a") as flag_file:
-                    flag_file.write(flag)
-                    flag_file.write("\n")
-                    input("got one, press enter to continue")
-                if len(secret_flags) == 5:
-                    print("all flags found")
-                    print(secret_flags)
-                    sys.exit(1)
+                if flag not in secret_flags:
+                    secret_flags.append(flag)
+                    if len(secret_flags) == 5:
+                        for secret_flag in secret_flags:
+                            print(secret_flag)
+                        sys.exit(1)
 
-        # Get tags from current_page
-        # for tag in tags:
-        #     # If tag is an anchor and that anchor has not been explored, add it to unexplored
-        #     if tag['type'] == 'a' and tag['href'] not in explored:
-        #         unexplored.append(tag['href'])
-        #
-        #     # If tag is an h2 and has class of 'secret_flag', add its contents to secret_flags
-        #     elif tag['type'] == 'h2' and tag['class'] == 'secret_flag':
-        #         secret_flags.append(tag['content'])
-
-        # Add current_page to explored
-        explored.append(current_page)
-        # print('Unexplored:', unexplored)
-        # print('Explored:', explored)
-        print('\n')
-
-
-#if postLogin returns true aka it got a 302, then begin crawl
+# If postLogin returns true (aka it got a 302), then begin crawl
 if postLogin():
     crawl()
 s.close()
-
-# print('Unexplored:', unexplored)
-print('Secret flags:', secret_flags)
-print('Explored:', len(explored))
